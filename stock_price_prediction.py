40002a181f8a78ce16e6a22058129d48bd8dcc7b
VinamraGupta <60665195+VinamraGuptaa@users.noreply.github.com>
2021-10-02 13:19:31 +0530

    Update stock_price_prediction.py

--- stock_price_prediction.py
+++ stock_price_prediction.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import numpy as np
 import yfinance as yf
+import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
